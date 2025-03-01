f499260ff3e9f42d3bb4c0eb053444c52719cb36
Adriano Cola <adriano@ensemble.com.br>
2025-03-01 08:59:49 -0300

    add more cryptos

--- src/components/ui/IconCrypto.tsx
+++ src/components/ui/IconCrypto.tsx
@@ -2,9 +2,15 @@ import * as React from 'react';
 import {SvgProps} from 'react-native-svg';
 
 import AdaSvg from '@/assets/icons/crypto/ada.svg';
+import BnbSvg from '@/assets/icons/crypto/bnb.svg';
 import BtcSvg from '@/assets/icons/crypto/btc.svg';
+import DogeSvg from '@/assets/icons/crypto/doge.svg';
 import EthSvg from '@/assets/icons/crypto/eth.svg';
 import LtcSvg from '@/assets/icons/crypto/ltc.svg';
+import SolSvg from '@/assets/icons/crypto/sol.svg';
+import TrxSvg from '@/assets/icons/crypto/trx.svg';
+import UsdcSvg from '@/assets/icons/crypto/usdc.svg';
+import UsdtSvg from '@/assets/icons/crypto/usdt.svg';
 import XrpSvg from '@/assets/icons/crypto/xrp.svg';
 import {CryptosEnum} from '@/constants/Cryptos';
 
@@ -16,8 +22,14 @@ const cryptoIconsMap: Record<CryptosEnum, React.FC<SvgProps>> = {
   [CryptosEnum.BTC]: BtcSvg,
   [CryptosEnum.ETH]: EthSvg,
   [CryptosEnum.ADA]: AdaSvg,
-  [CryptosEnum.LTC]: LtcSvg,
   [CryptosEnum.XRP]: XrpSvg,
+  [CryptosEnum.LTC]: LtcSvg,
+  [CryptosEnum.BNB]: BnbSvg,
+  [CryptosEnum.SOL]: SolSvg,
+  [CryptosEnum.DOGE]: DogeSvg,
+  [CryptosEnum.TRX]: TrxSvg,
+  [CryptosEnum.USDT]: UsdtSvg,
+  [CryptosEnum.USDC]: UsdcSvg,
 };
 
 export const IconCrypto = ({crypto, ...props}: CryptoIconProps) => {
--- src/constants/Cryptos.ts
+++ src/constants/Cryptos.ts
@@ -4,6 +4,12 @@ export enum CryptosEnum {
   ADA = 'ADA',
   XRP = 'XRP',
   LTC = 'LTC',
+  BNB = 'BNB',
+  SOL = 'SOL',
+  DOGE = 'DOGE',
+  TRX = 'TRX',
+  USDT = 'USDT',
+  USDC = 'USDC',
 }
 
 export interface CryptoData {
@@ -18,6 +24,12 @@ export const CryptoUsdPrices: Record<CryptosEnum, number> = {
   [CryptosEnum.ADA]: 0.631,
   [CryptosEnum.XRP]: 2.14,
   [CryptosEnum.LTC]: 127.72,
+  [CryptosEnum.BNB]: 592.49,
+  [CryptosEnum.SOL]: 140.48,
+  [CryptosEnum.DOGE]: 0.203,
+  [CryptosEnum.TRX]: 0.2327,
+  [CryptosEnum.USDT]: 0.9996,
+  [CryptosEnum.USDC]: 0.9999,
 };
 
 export const Cryptos: Record<CryptosEnum, CryptoData> = {
@@ -45,5 +57,35 @@ export const Cryptos: Record<CryptosEnum, CryptoData> = {
     name: 'Litecoin',
     symbol: CryptosEnum.LTC,
     color: '#bfbbbb',
+  },
+  [CryptosEnum.BNB]: {
+    name: 'BNB',
+    symbol: CryptosEnum.BNB,
+    color: '#f3ba2f',
+  },
+  [CryptosEnum.SOL]: {
+    name: 'Solana',
+    symbol: CryptosEnum.SOL,
+    color: '#66f9a1',
+  },
+  [CryptosEnum.DOGE]: {
+    name: 'Dogecoin',
+    symbol: CryptosEnum.DOGE,
+    color: '#c3a635',
+  },
+  [CryptosEnum.TRX]: {
+    name: 'TRON',
+    symbol: CryptosEnum.TRX,
+    color: '#ef2026',
+  },
+  [CryptosEnum.USDT]: {
+    name: 'Tether',
+    symbol: CryptosEnum.USDT,
+    color: '#26a17b',
+  },
+  [CryptosEnum.USDC]: {
+    name: 'USDC',
+    symbol: CryptosEnum.USDC,
+    color: '#3e73c4',
   },
 } as const;
